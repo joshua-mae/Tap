686cc9e6993bcfb8f8c85b88e32627af057d8800
joshua-mae <86929441+joshua-mae@users.noreply.github.com>
2023-05-08 23:01:33 -0400

    Starting Files

---
+++ tap/tap/tapApp.swift
@@ -0,0 +1,16 @@
+//
+//  tapApp.swift
+//  tap
+//
+//
+
+import SwiftUI
+
+@main
+struct tapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
